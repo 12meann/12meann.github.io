76a4c4c6ba13f41b7a5e96bfadc983a4c771c7e5
Me-ann Urgel <meann1_2@yahoo.com>
2019-01-16 05:56:21 +0800

    Landing page to skills page done

---
+++ dist/js/app.js
@@ -0,0 +1,34 @@
+// sticky header
+
+window.onscroll = function() {
+  myFunction();
+};
+
+var header = document.querySelector(".container");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// skills pie chart
+
+$(function() {
+  $(".chart").easyPieChart({
+    scaleColor: false,
+    size: 180,
+    barColor: "#ffc107",
+    lineWidth: 15,
+    trackColor: "#373737",
+    lineCap: "circle",
+    animate: 2000
+  });
+});
+
+function newFunction() {
+  return "black";
+}
