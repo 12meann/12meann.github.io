fb8b132eb7bbcd72a286cc5c840e74c14393fa32
Me-ann Urgel <meann1_2@yahoo.com>
2019-01-19 16:02:11 +0800

    added tooltip and smooth scroll

--- dist/js/app.js
+++ dist/js/app.js
@@ -32,3 +32,22 @@ $(function() {
 function newFunction() {
   return "black";
 }
+
+// Smooth Scrolling
+$(".links a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
